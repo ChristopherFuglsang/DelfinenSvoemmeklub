b74d4cf6b9b314ddb327dee9977ebbcf14bb3850
Krelle25 <kmeyersteffensen@gmail.com>
2024-12-04 13:01:21 +0100

    Update CprNr.java

--- CprNr.java
+++ CprNr.java
@@ -4,11 +4,12 @@ public class CprNr
 {
     String nummeret;
 
-    public CprNr() {}
+    public CprNr()
+    {}
 
-    public CprNr(String n)
+    public CprNr(String etCprNr)
     {
-        nummeret = n;
+        nummeret = etCprNr;
     }
 
     public String getCprNr()
@@ -16,19 +17,21 @@ public class CprNr
         return nummeret;
     }
 
-    public void setCprNr(String n)
+    public void setCprNr(String etCprNr)
     {
-        nummeret = n;
+        nummeret = etCprNr;
     }
 
     public int getDag()
     {
-        return Integer.parseInt(nummeret.substring(0,2));
+        int d = Integer.parseInt(nummeret.substring(0,2));
+        return d;
     }
 
     public int getMaaned()
     {
-        return Integer.parseInt(nummeret.substring(2,4));
+        int m = Integer.parseInt(nummeret.substring(2,4));
+        return m;
     }
 
     public int getAar()
@@ -37,17 +40,16 @@ public class CprNr
         if (nummeret.charAt(6) >= '0' && nummeret.charAt(6) <= '3')
             aarhundrede = 1900;
         else
-        if (nummeret.charAt(6) == '4' || nummeret.charAt(6) == '5')
-            aarhundrede = 1800;
-        else
             aarhundrede = 2000;
 
         return aarhundrede + Integer.parseInt(nummeret.substring(4,6));
     }
 
+    // Precondition: nummeret indeholder en korrekt dato
     public Dato getDato()
     {
         Dato d = new Dato(getAar(),getMaaned(),getDag());
+
         return d;
     }
 
@@ -55,15 +57,13 @@ public class CprNr
     {
         LocalDate dd = LocalDate.now();
         Dato iDag = new Dato(dd.getYear(),dd.getMonthValue(),dd.getDayOfMonth());
-
         return (iDag.getDatoen() - getDato().getDatoen()) / 10000;
     }
 
     public boolean erMand()
     {
-        if (Integer.parseInt(nummeret.substring(9,10)) % 2 != 0)
-            return true;
-        return false;
+        int n = Integer.parseInt(nummeret.substring(9,10));
+        return n % 2 == 1;
     }
 
     public boolean erKvinde()
@@ -76,19 +76,18 @@ public class CprNr
         if (nummeret.length() != 10)
             return false;
 
-        for (int i = 0;i < 10; i++)
+        for (int i = 0; i < 10; i++)
             if (nummeret.charAt(i) < '0' || nummeret.charAt(i) > '9')
                 return false;
 
-        if (!getDato().validerDato())
+        Dato d = getDato();
+        if (!d.validerDato())
             return false;
 
         int udregnet = 0;
         int[] vaegte = {4,3,2,7,6,5,4,3,2,1};
-
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10;i++)
             udregnet += vaegte[i] * Integer.parseInt(""+nummeret.charAt(i));
-        //udregnet += vaegte[i] * (nummeret.charAt(i) - '0');
         if (udregnet % 11 != 0)
             return false;
 
